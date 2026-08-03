adbae42e8eb34eb142fcf54e5c27c70e51b9d116
Rashid usmani <rashidusmani506@gmail.com>
2026-08-03 11:00:00 +0530

    signin signup apis and frontend

---
+++ Backend/FASTAPI/post_apis/Users_api/Signin_api.py
@@ -0,0 +1,80 @@
+from fastapi import FastAPI,HTTPException,Depends
+from sqlalchemy.orm import Session
+from sqlalchemy import or_,and_
+from fastapi.security import OAuth2PasswordBearer,OAuth2PasswordRequestForm
+from passlib.context import CryptContext
+from jose import jwt,JWTError
+from datetime import datetime,timedelta,timezone
+from DATABASE.Tables.users_table import User
+from FASTAPI.my_seceret_key import SECRET_KEY,ALGORITHM,TOKEN_EXPIRY_MIN
+from DATABASE.database import get_db
+
+app = FastAPI()
+
+password_context = CryptContext(schemes=["bcrypt"])
+
+Oauth2_schemes = OAuth2PasswordBearer(tokenUrl="signin")
+
+def Hash_pass(password:str):
+    return password_context.hash(password)
+
+def verify_pass(plain_password,hashed_password):
+    return password_context.verify(plain_password,hashed_password)
+
+def create_token(data:dict):
+    to_encode = data.copy()
+
+    expire = datetime.now(timezone.utc) + timedelta(minutes=TOKEN_EXPIRY_MIN)
+
+    to_encode.update({
+        "exp" : expire
+    })
+
+    token = jwt.encode(
+        to_encode,
+        SECRET_KEY,
+        algorithm=ALGORITHM
+    )
+    return token
+
+@app.post("/signin")
+def signin(form_data : OAuth2PasswordRequestForm = Depends(),db:Session = Depends(get_db)):
+    user = db.query(User).filter(User.username == form_data.username).first()
+
+    if not user or not verify_pass(form_data.password,user.password_hash):
+        raise HTTPException(
+            status_code=400,
+            detail="Invalid username or password "
+        )
+    ACCESS_TOKEN = create_token({"sub":form_data.username})
+    return{
+        "access_token" : ACCESS_TOKEN,
+        "token_type" : "bearer"
+    }
+
+
+def verify_token(token:str = Depends(Oauth2_schemes)):
+    try :
+        payload = jwt.decode(token,SECRET_KEY,algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None :
+            raise HTTPException(
+                status_code=401,
+                detail="Invalid token"
+            )
+        return username
+    except jwt.JWTError:
+            raise HTTPException(
+                status_code= 401,
+                detail="Invalid token"
+            )
+
+# Protected Route
+@app.get("/protected")
+def protected_route(username: str = Depends(verify_token)):
+    return{
+        "message" : f"Hello {username}, you have access to this protected route",
+        "user" : username,
+        "password" : "this is protected "
+    }
+        
--- Frontend/Frontend/src/App.jsx
+++ Frontend/Frontend/src/App.jsx
@@ -4,15 +4,32 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 import Navbar from './components/navbar/navbar.jsx'
+import homepage from './pages/homepage/homepage.jsx'
+import Homepage from './pages/homepage/homepage.jsx'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import Signin from './pages/signin.jsx'
+
+const router = createBrowserRouter(
+  [
+    {path : "/",
+      element : <>
+      <Navbar/>,
+      <Homepage/>
+      </>
+    },
+    {path : "/signin",
+      element : <Signin/>
+    }
+  ]
+)
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <Navbar/>
-    
-    <div className='h-screen bg-amber-500'></div>
+    {/* <Homepage/> */}
+    <RouterProvider router={router}/>
     </>
   )
 }
